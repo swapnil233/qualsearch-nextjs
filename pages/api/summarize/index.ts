c06cd7114acbdedcac1c6b39ff8e868ef0b8198e
Hasan Iqbal <swapniliqbal@gmail.com>
2023-06-21 23:30:17 -0400

    refactor: secured the /api/summarize API endpoint

--- pages/api/summarize/index.ts
+++ pages/api/summarize/index.ts
@@ -3,12 +3,20 @@ import {
     IOpenAIApi
 } from "@/utils/openAI";
 import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 const { Configuration, OpenAIApi } = require("openai");
 
 export default async function Handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    const session = await getServerSession(req, res, authOptions);
+
+    if (!session) {
+        return res.status(401).send("Unauthorized");
+    }
+
     // POST '/api/summarize/'
     if (req.method === "POST") {
         const { transcription } = req.body;
