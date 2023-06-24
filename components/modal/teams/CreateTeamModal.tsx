1aa9d5788408b13693f0fd939d13ca62f944366f
Hasan Iqbal <swapniliqbal@gmail.com>
2023-06-24 00:32:07 -0400

    feat: implement Card and Modal components

---
+++ components/modal/teams/CreateTeamModal.tsx
@@ -0,0 +1,87 @@
+import {
+  Alert,
+  Button,
+  Group,
+  Modal,
+  TextInput,
+  Textarea,
+  Title,
+  rem,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { IconAlertCircle } from "@tabler/icons-react";
+import { FC } from "react";
+
+export interface ICreateTeamModal {
+  opened: boolean;
+  close: () => void;
+  creating: boolean;
+  form: ReturnType<
+    typeof useForm<{ teamName: string; teamDescription: string }>
+  >;
+  handleCreateNewTeam: (
+    _values: { teamName: string; teamDescription: string },
+    _event: React.FormEvent
+  ) => void;
+}
+
+const CreateTeamModal: FC<ICreateTeamModal> = ({
+  opened,
+  close,
+  creating,
+  form,
+  handleCreateNewTeam,
+}) => {
+  return (
+    <Modal
+      opened={opened}
+      onClose={close}
+      title={<Title order={3}>Create a new team</Title>}
+      centered
+      padding={"lg"}
+    >
+      <form onSubmit={form.onSubmit(handleCreateNewTeam)}>
+        <TextInput
+          placeholder="ABC Example Team"
+          label="Team name"
+          description="This name can be changed later in team settings"
+          radius="xs"
+          withAsterisk
+          mb={rem(40)}
+          size="md"
+          {...form.getInputProps("teamName")}
+        />
+
+        <Textarea
+          placeholder="A description about this team"
+          label="Team description"
+          description="A description will help your team members identify the team"
+          radius="xs"
+          size="md"
+          mb={rem(20)}
+          {...form.getInputProps("teamDescription")}
+        />
+
+        <Alert
+          icon={<IconAlertCircle size="1rem" />}
+          title="You are creating an empty team"
+          radius="xs"
+          mb={rem(20)}
+        >
+          You can add team members after creating the team.
+        </Alert>
+
+        <Group position="apart">
+          <Button variant="subtle" radius="xs" onClick={close}>
+            Cancel
+          </Button>
+          <Button radius="xs" type="submit" loading={creating}>
+            Create
+          </Button>
+        </Group>
+      </form>
+    </Modal>
+  );
+};
+
+export default CreateTeamModal;
