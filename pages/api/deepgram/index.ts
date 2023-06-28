fb8a35062dd33a0b91bbe4211833dea397902f34
Hasan Iqbal <swapniliqbal@gmail.com>
2023-06-27 21:49:53 -0400

    refactor: comments and checks in deepgram api code

--- pages/api/deepgram/index.ts
+++ pages/api/deepgram/index.ts
@@ -5,10 +5,26 @@ export default async function Handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    if (req.method === "POST") {
+
+    if (req.method !== "POST") {
+        res.status(405).send("Method not allowed.");
+    }
+
+    try {
         const { DEEPGRAM_API_KEY } = process.env;
+
+        if (!DEEPGRAM_API_KEY) {
+            res.status(500).json({ message: "Deepgram API key not found" });
+            return;
+        }
+
         const deepgram = new Deepgram(DEEPGRAM_API_KEY);
         const { uri } = req.body;
+
+        if (!uri) {
+            res.status(400).json({ message: "No URI provided" });
+            return;
+        }
 
         const audioSource = {
             url: uri as string,
@@ -25,7 +41,8 @@ export default async function Handler(
         const response = await deepgram.transcription.preRecorded(audioSource, options);
 
         res.status(200).json(response);
-    } else {
-        res.status(405).json({ message: "Method not allowed" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "An unexpected error occured" });
     }
 }
