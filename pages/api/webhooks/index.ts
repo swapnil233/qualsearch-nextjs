5c8c7b0e42abbde7a217d78d7b922f2c6f91cfcc
Hasan Iqbal <swapniliqbal@gmail.com>
2023-06-28 23:32:30 -0400

    feat: started implementing webhook API endpoint for Deepgram

---
+++ pages/api/webhooks/index.ts
@@ -0,0 +1,12 @@
+import Cors from 'micro-cors';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const cors = Cors({
+    allowMethods: ['POST', 'HEAD'],
+});
+
+const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+    return res.status(200).json({ message: 'ok' });
+};
+
+export default cors(webhookHandler as any);
