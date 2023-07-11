e76a3a11093ef01ba92b5847b166bd8dcf4f9eaa
Hasan Iqbal <swapniliqbal@gmail.com>
2023-07-11 00:27:09 -0400

    test: dg test

--- pages/api/webhooks/index.ts
+++ pages/api/webhooks/index.ts
@@ -1,3 +1,5 @@
+import { ErrorMessages } from "@/constants/ErrorMessages";
+import { HttpStatus } from "@/constants/HttpStatus";
 import prisma from "@/utils/prisma";
 import { File } from "@prisma/client";
 import Cors from "micro-cors";
@@ -10,7 +12,7 @@ const cors = Cors({
 const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   /**
      req.body = {
-        metadata: {
+      metadata: {
         transaction_key: 'deprecated',
         request_id: '562827bd-de8e-4fb6-8678-fa3a7e55de24',
         sha256: '380ece2b282ca047850648243a6eb0a713de9678ec42a656b49e017e15b33359',
@@ -21,30 +23,40 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
         model_info: { '85b5dbfd-bc1d-46b5-99d8-070d379a7f97': [Object] },
         tags: [ 'cljxqx7bg0007r9sw9k6qs61o-cljxqxi850009r9sw5rqb65hp' ]
       },
+      
       results: { channels: [ [Object] ] }
     }
    */
 
-  const { request_id, tags } = req.body.metadata;
-  const teamId = tags[0].split("-")[0];
-  const projectId = tags[0].split("-")[1];
+  const { request_id } = req.body.metadata;
+  console.log("DG Request ID", request_id)
 
-  const files: ({ files: File[] }) = await prisma.project.findUniqueOrThrow({
-    where: {
-      id: projectId
-    },
-    select: {
-      files: {
-        where: {
-          dgCallbackRequestId: request_id
-        },
+  try {
+    const fileToUpdate: File = await prisma.file.findUniqueOrThrow({
+      where: {
+        dgCallbackRequestId: request_id
       }
+    })
+
+    console.log("File to update", fileToUpdate)
+
+    const updatedFile = await prisma.file.update({
+      where: {
+        id: fileToUpdate.id
+      },
+      data: {
+        transcript: req.body
+      }
+    })
+
+    res.status(HttpStatus.Ok).send(updatedFile);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      res.status(HttpStatus.InternalServerError).send(error.message);
+    } else {
+      res.status(HttpStatus.InternalServerError).send(ErrorMessages.InternalServerError)
     }
-  })
-
-  const file: File = files.files[0]
-
-  console.log(file)
+  }
 };
 
 export default cors(webhookHandler as any);
