090a1c7a039a3e7f287a55a093c93ccdea14d95a
Hasan Iqbal <swapniliqbal@gmail.com>
2023-07-11 00:10:26 -0400

    test: dg cb test

--- pages/api/webhooks/index.ts
+++ pages/api/webhooks/index.ts
@@ -1,4 +1,5 @@
 import prisma from "@/utils/prisma";
+import { File } from "@prisma/client";
 import Cors from "micro-cors";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -10,32 +11,40 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   /**
      req.body = {
         metadata: {
-          transaction_key: 'deprecated',
-          request_id: '8b4da3e8-a730-4e99-8b6a-f43afc41a286',
-          sha256: '380ece2b282ca047850648243a6eb0a713de9678ec42a656b49e017e15b33359',
-          created: '2023-07-11T03:42:40.415Z',
-          duration: 578.904,
-          channels: 1,
-          models: [ '85b5dbfd-bc1d-46b5-99d8-070d379a7f97' ],
-          model_info: { '85b5dbfd-bc1d-46b5-99d8-070d379a7f97': [Object] }
-        },
-        results: { channels: [ [Object] ] }
+        transaction_key: 'deprecated',
+        request_id: '562827bd-de8e-4fb6-8678-fa3a7e55de24',
+        sha256: '380ece2b282ca047850648243a6eb0a713de9678ec42a656b49e017e15b33359',
+        created: '2023-07-11T04:00:59.109Z',
+        duration: 578.904,
+        channels: 1,
+        models: [ '85b5dbfd-bc1d-46b5-99d8-070d379a7f97' ],
+        model_info: { '85b5dbfd-bc1d-46b5-99d8-070d379a7f97': [Object] },
+        tags: [ 'cljxqx7bg0007r9sw9k6qs61o-cljxqxi850009r9sw5rqb65hp' ]
+      },
+      results: { channels: [ [Object] ] }
     }
    */
 
-  const { request_id } = req.body.metadata
+  const { request_id, tags } = req.body.metadata;
+  const teamId = tags[0].split("-")[0];
+  const projectId = tags[0].split("-")[1];
 
-  // Check if there's a file in the DB that has the dgCallbackRequestId equal to req.body's request_id
-  const fileRef = await prisma.file.groupBy({
-    by: ['dgCallbackRequestId'],
+  const files: ({ files: File[] }) = await prisma.project.findUniqueOrThrow({
     where: {
-      dgCallbackRequestId: {
-        equals: request_id
+      id: projectId
+    },
+    select: {
+      files: {
+        where: {
+          id: request_id
+        },
       }
     }
   })
 
-  console.log("fileRef: ", fileRef)
+  const file: File = files.files[0]
+
+  console.log(file)
 };
 
 export default cors(webhookHandler as any);
