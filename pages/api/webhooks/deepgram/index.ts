1afdc32f270c3fba4ff353d213d18b3ceec1b573
Hasan Iqbal <swapniliqbal@gmail.com>
2023-07-15 15:58:57 -0400

    refactor: removed example request body comment

--- pages/api/webhooks/deepgram/index.ts
+++ pages/api/webhooks/deepgram/index.ts
@@ -9,50 +9,35 @@ const cors = Cors({
 });
 
 const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-
-  /**
-     req.body = {
-      metadata: {
-        transaction_key: 'deprecated',
-        request_id: '562827bd-de8e-4fb6-8678-fa3a7e55de24',
-        sha256: '380ece2b282ca047850648243a6eb0a713de9678ec42a656b49e017e15b33359',
-        created: '2023-07-11T04:00:59.109Z',
-        duration: 578.904,
-        channels: 1,
-        models: [ '85b5dbfd-bc1d-46b5-99d8-070d379a7f97' ],
-        model_info: { '85b5dbfd-bc1d-46b5-99d8-070d379a7f97': [Object] },
-        tags: [ 'cljxqx7bg0007r9sw9k6qs61o-cljxqxi850009r9sw5rqb65hp' ]
-      },
-      
-      results: { channels: [ [Object] ] }
-    }
-   */
-
   // Only allow POST
-  if (req.method !== "POST") return res.status(HttpStatus.MethodNotAllowed).send(ErrorMessages.MethodNotAllowed);
+  if (req.method !== "POST")
+    return res
+      .status(HttpStatus.MethodNotAllowed)
+      .send(ErrorMessages.MethodNotAllowed);
 
   const { request_id } = req.body.metadata;
-  if (!request_id) return res.status(HttpStatus.BadRequest).send(ErrorMessages.BadRequest);
+  if (!request_id)
+    return res.status(HttpStatus.BadRequest).send(ErrorMessages.BadRequest);
 
   const dgResponse = req.body.results.channels[0].alternatives[0];
   const metadata = req.body.metadata;
 
   try {
-
     // Find the file that has this request ID
-    const fileWithThisRequestId = await prisma.deepgramTranscriptRequest.findUnique({
-      where: {
-        request_id: request_id
-      },
-      select: {
-        file: true
-      }
-    })
+    const fileWithThisRequestId =
+      await prisma.deepgramTranscriptRequest.findUnique({
+        where: {
+          request_id: request_id,
+        },
+        select: {
+          file: true,
+        },
+      });
 
     if (!fileWithThisRequestId?.file) {
       return res
         .status(HttpStatus.BadRequest)
-        .send("No file associated with this request ID")
+        .send("No file associated with this request ID");
     }
 
     // Create a new Transcript row with the data
@@ -74,39 +59,38 @@ const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
             channels: metadata.channels,
             duration: metadata.duration,
             model_info: metadata.model_info,
-            request_id: request_id
-          }
+            request_id: request_id,
+          },
         },
         file: {
           connect: {
-            id: fileWithThisRequestId.file.id
-          }
-        }
-      }
-    })
+            id: fileWithThisRequestId.file.id,
+          },
+        },
+      },
+    });
 
     // Update the file's status to COMPLETED
     const updatedFile = await prisma.file.update({
       where: {
-        id: fileWithThisRequestId.file.id
+        id: fileWithThisRequestId.file.id,
       },
       data: {
-        status: "COMPLETED"
-      }
-    })
+        status: "COMPLETED",
+      },
+    });
 
     return res.status(HttpStatus.Ok).send({
       fileWithThisRequestId: fileWithThisRequestId.file.id,
       newTranscriptId: newTranscript.id,
-      updatedFileStatus: updatedFile.status
-    })
-
+      updatedFileStatus: updatedFile.status,
+    });
   } catch (error: unknown) {
     if (error instanceof Error) {
-      console.log(error)
+      console.log(error);
       res.status(HttpStatus.InternalServerError).send(error.message);
     } else {
-      console.log(error)
+      console.log(error);
       res
         .status(HttpStatus.InternalServerError)
         .send(ErrorMessages.InternalServerError);
