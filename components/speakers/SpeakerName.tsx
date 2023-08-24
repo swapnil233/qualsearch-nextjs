3c1c79f8864f18eabf1f7479c5cd01402f5974c6
Hasan Iqbal <swapniliqbal@gmail.com>
2023-08-24 03:27:31 -0400

    refactor: abstract away speaker names to its own component

---
+++ components/speakers/SpeakerName.tsx
@@ -0,0 +1,86 @@
+import { IGroup } from "@/utils/groupTranscriptBySpeaker";
+import { Button, Popover, Text, TextInput } from "@mantine/core";
+import { FC, useState } from "react";
+
+interface ISpeakerName {
+  group: IGroup;
+}
+
+export const SpeakerName: FC<ISpeakerName> = ({ group }) => {
+  const [speakerNames, setSpeakerNames] = useState<Record<number, string>>({});
+  const [newSpeakerName, setNewSpeakerName] = useState<string>("");
+
+  /**
+   * Handle the change of the speaker name by updating the state and clearing the input
+   * @param speaker
+   * @param name
+   */
+  const handleSpeakerNameChange = (speaker: number, name: string) => {
+    setSpeakerNames((prev) => ({
+      ...prev,
+      [speaker]: name,
+    }));
+
+    setNewSpeakerName("");
+  };
+
+  return (
+    <Popover
+      width={300}
+      trapFocus
+      position="bottom-start"
+      withArrow
+      shadow="lg"
+    >
+      <Popover.Target>
+        <Text
+          color="#190041"
+          fs={"1.2rem"}
+          mt={"2rem"}
+          mb={"8px"}
+          fw={"bold"}
+          w={"100%"}
+          sx={{
+            cursor: "pointer",
+            ":hover": {
+              textDecoration: "underline",
+            },
+          }}
+        >
+          {/* Either use the speaker name from the state or the speaker name from the transcript */}
+          {speakerNames[group.speaker] || `Speaker ${group.speaker + 1}`}
+        </Text>
+      </Popover.Target>
+      <Popover.Dropdown
+        sx={(theme) => ({
+          background:
+            theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+        })}
+      >
+        <TextInput
+          label="Change speaker name"
+          placeholder={
+            speakerNames[group.speaker] || `Speaker ${group.speaker + 1}`
+          }
+          mb="md"
+          value={newSpeakerName}
+          onChange={(e) => {
+            setNewSpeakerName(e.target.value);
+            // Close the popover after setting the new speaker name
+          }}
+        />
+        <Button
+          size="xs"
+          variant="light"
+          onClick={() => {
+            if (newSpeakerName !== "") {
+              handleSpeakerNameChange(group.speaker, newSpeakerName);
+            }
+          }}
+        >
+          Change
+        </Button>
+      </Popover.Dropdown>
+    </Popover>
+  );
+};
