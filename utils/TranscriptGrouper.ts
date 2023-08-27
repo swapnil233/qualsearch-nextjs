b5b3dc3b56e941bb1e1f5ab424897d96caab8812
Hasan Iqbal <swapniliqbal@gmail.com>
2023-08-27 04:46:14 -0400

    feat: switched to a class to implement word grouping by user

---
+++ utils/TranscriptGrouper.ts
@@ -0,0 +1,33 @@
+import { IGroup, ITranscript } from "@/components/transcript/interfaces";
+
+export class TranscriptGrouper {
+    transcript: ITranscript[];
+
+    constructor(transcript: ITranscript[]) {
+        this.transcript = transcript
+    }
+
+    groupBySpeaker(): IGroup[] {
+        const groupedTranscript: IGroup[] = [];
+
+        for (let i = 0; i < this.transcript.length; i++) {
+            const word = this.transcript[i];
+            if (
+                groupedTranscript.length === 0 ||
+                groupedTranscript[groupedTranscript.length - 1].speaker !== word.speaker
+            ) {
+                groupedTranscript.push({
+                    speaker: word.speaker,
+                    words: [{ ...word, index: i }],
+                });
+            } else {
+                groupedTranscript[groupedTranscript.length - 1].words.push({
+                    ...word,
+                    index: i,
+                });
+            }
+        }
+
+        return groupedTranscript;
+    }
+}
