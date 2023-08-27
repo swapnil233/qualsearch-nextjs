695330ae7f1a2f6a7d573956b158dfbab528daa1
Hasan Iqbal <swapniliqbal@gmail.com>
2023-08-27 18:48:13 -0400

    fix: fixed flexBox issue on note card

--- components/note/NoteCard.tsx
+++ components/note/NoteCard.tsx
@@ -1,5 +1,15 @@
 import { NotesAndUsers } from "@/types";
-import { Avatar, Box, Group, Paper, Stack, Text } from "@mantine/core";
+import {
+  ActionIcon,
+  Avatar,
+  Box,
+  Group,
+  Menu,
+  Paper,
+  Stack,
+  Text,
+} from "@mantine/core";
+import { IconDots, IconEdit, IconTrash } from "@tabler/icons-react";
 
 interface INoteCardProps {
   position: {
@@ -17,7 +27,6 @@ export function NoteCard({ position, note }: INoteCardProps) {
         position: "absolute",
         top: position.top,
         left: position.left,
-        zIndex: 10,
       }}
     >
       <Paper
@@ -27,24 +36,43 @@ export function NoteCard({ position, note }: INoteCardProps) {
         top={position.top}
         left={position.left}
       >
-        <Group align="flex-start">
-          <Stack justify="flex-start" align="flex-start">
-            <Avatar
-              src={note.createdBy.image}
-              alt={note.createdBy.name || ""}
-              radius="xl"
-            />
+        <Stack align="flex-start">
+          <Stack justify="flex-start" align="flex-start" w={"100%"}>
+            <Group position="apart" w={"100%"}>
+              <Group>
+                <Avatar
+                  src={note.createdBy.image}
+                  alt={note.createdBy.name || ""}
+                  radius="xl"
+                />
+                <Stack spacing={0}>
+                  <Text fz="md">{note.createdBy.name}</Text>
+                  <Text fz="xs" c="dimmed">
+                    {new Date(note.createdAt).toDateString()}
+                  </Text>
+                </Stack>
+              </Group>
+
+              <Menu shadow="md" width={200}>
+                <Menu.Target>
+                  <ActionIcon variant="transparent">
+                    <IconDots size="1rem" />
+                  </ActionIcon>
+                </Menu.Target>
+
+                <Menu.Dropdown>
+                  <Menu.Item icon={<IconEdit size={14} />}>Edit</Menu.Item>
+                  <Menu.Item color="red" icon={<IconTrash size={14} />}>
+                    Delete
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </Group>
           </Stack>
           <Stack justify="flex-start">
-            <Stack spacing={0}>
-              <Text fz="md">{note.createdBy.name}</Text>
-              <Text fz="xs" c="dimmed">
-                {new Date(note.createdAt).toDateString()}
-              </Text>
-            </Stack>
             <Text fz={"sm"}>{note.text}</Text>
           </Stack>
-        </Group>
+        </Stack>
       </Paper>
     </Box>
   );
