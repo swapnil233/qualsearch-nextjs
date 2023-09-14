f9b6d5d7a0768f404f5895f6683d86b0910a07c6
Hasan Iqbal <swapniliqbal@gmail.com>
2023-09-14 01:55:08 -0400

    refactor: improved styling and performance

--- components/aside/TranscriptPageAside.tsx
+++ components/aside/TranscriptPageAside.tsx
@@ -2,7 +2,9 @@ import { NoteWithTagsAndCreator, TagWithNotes } from "@/types";
 import {
   Aside,
   Box,
+  Button,
   Center,
+  Divider,
   Group,
   NativeSelect,
   ScrollArea,
@@ -16,11 +18,14 @@ import {
 import { useDebouncedState } from "@mantine/hooks";
 import { User } from "@prisma/client";
 import {
+  IconDownload,
   IconNote,
   IconSearch,
   IconSparkles,
   IconTags,
+  IconUpload,
 } from "@tabler/icons-react";
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import EmptyState from "../states/empty/EmptyState";
 import { AsideNotes } from "./AsideNotes";
@@ -28,14 +33,16 @@ import { AsideTags } from "./AsideTags";
 
 interface ITranscriptPageAside {
   notes: NoteWithTagsAndCreator[];
+  user: User;
+
   tags: TagWithNotes[];
   setTags: React.Dispatch<React.SetStateAction<TagWithNotes[]>>;
+
   segment: "tags" | "notes" | "chat";
   setSegment: React.Dispatch<React.SetStateAction<"tags" | "notes" | "chat">>;
+
   mediaRef: React.MutableRefObject<HTMLAudioElement | HTMLVideoElement | null>;
   transcriptContainerDivRef: React.RefObject<HTMLDivElement>;
-  projectId: string;
-  user: User;
 }
 
 type SortCategories = "newest-to-oldest" | "oldest-to-newest" | "chronological";
@@ -48,12 +55,12 @@ export const TranscriptPageAside: React.FC<ITranscriptPageAside> = ({
   setTags,
   mediaRef,
   transcriptContainerDivRef,
-  projectId,
   user,
 }) => {
   const theme = useMantineTheme();
 
-  console.log("notes", notes);
+  const router = useRouter();
+  const projectId = router.query.projectId as string;
 
   const [filteredNotes, setFilteredNotes] =
     useState<NoteWithTagsAndCreator[]>(notes);
@@ -61,6 +68,7 @@ export const TranscriptPageAside: React.FC<ITranscriptPageAside> = ({
 
   const [sort, setSort] = useState<SortCategories>();
 
+  // Filter notes by search term
   useEffect(() => {
     const filterNotesBySearchTerm = () => {
       if (searchTerm.length > 0) {
@@ -77,6 +85,7 @@ export const TranscriptPageAside: React.FC<ITranscriptPageAside> = ({
     setFilteredNotes(filteredNotes);
   }, [searchTerm, notes]);
 
+  // Sort notes by sort category
   useEffect(() => {
     let sortedNotes = [...filteredNotes];
     switch (sort) {
@@ -115,115 +124,141 @@ export const TranscriptPageAside: React.FC<ITranscriptPageAside> = ({
           : "rgb(249, 249, 248)"
       }
     >
-      <SegmentedControl
-        fullWidth
-        value={segment}
-        transitionDuration={100}
-        mb={rem(16)}
-        onChange={(value) => setSegment(value as "tags" | "notes" | "chat")}
-        data={[
-          {
-            value: "notes",
-            label: (
-              <Center>
-                <IconNote size="1rem" />
-                <Box ml={10}>Notes</Box>
-              </Center>
-            ),
-          },
-          {
-            value: "tags",
-            label: (
-              <Center>
-                <IconTags size="1rem" />
-                <Box ml={10}>Tags</Box>
-              </Center>
-            ),
-          },
-          {
-            value: "chat",
-            label: (
-              <Center>
-                <IconSparkles size="1rem" />
-                <Box ml={10}>Chat</Box>
-              </Center>
-            ),
-          },
-        ]}
-      />
+      <Stack>
+        <SegmentedControl
+          fullWidth
+          value={segment}
+          transitionDuration={100}
+          mb={rem(16)}
+          onChange={(value) => setSegment(value as "tags" | "notes" | "chat")}
+          data={[
+            {
+              value: "notes",
+              label: (
+                <Center>
+                  <IconNote size="1rem" />
+                  <Box ml={10}>Notes</Box>
+                </Center>
+              ),
+            },
+            {
+              value: "tags",
+              label: (
+                <Center>
+                  <IconTags size="1rem" />
+                  <Box ml={10}>Tags</Box>
+                </Center>
+              ),
+            },
+            {
+              value: "chat",
+              label: (
+                <Center>
+                  <IconSparkles size="1rem" />
+                  <Box ml={10}>Chat</Box>
+                </Center>
+              ),
+            },
+          ]}
+        />
 
-      {segment === "notes" && (
-        <>
-          {notes.length > 0 ? (
-            <>
-              <Group noWrap mb={16} align="start">
-                <TextInput
-                  icon={<IconSearch size="1rem" />}
-                  placeholder="Search..."
-                  onChange={(e) =>
-                    setSearchTerm(e.currentTarget.value.toLocaleLowerCase())
-                  }
-                />
-                <NativeSelect
-                  data={[
-                    { value: "newest-to-oldest", label: "Newest to oldest" },
-                    { value: "oldest-to-newest", label: "Oldest to newest" },
-                    { value: "chronological", label: "Chronological" },
-                  ]}
-                  value={sort}
-                  variant="filled"
-                  onChange={(e) =>
-                    setSort(e.currentTarget.value as SortCategories)
-                  }
-                />
-              </Group>
-              <ScrollArea h={"90%"}>
-                <AsideNotes
-                  mediaRef={mediaRef}
-                  transcriptContainerDivRef={transcriptContainerDivRef}
-                  notes={filteredNotes}
-                />
-                {searchTerm.length > 0 && filteredNotes.length === 0 && (
-                  <Text color="dimmed">No matching notes found...</Text>
+        {segment === "notes" && (
+          <>
+            {notes.length > 0 ? (
+              <>
+                <Group noWrap>
+                  <TextInput
+                    icon={<IconSearch size="1rem" />}
+                    placeholder="Search..."
+                    onChange={(e) =>
+                      setSearchTerm(e.currentTarget.value.toLocaleLowerCase())
+                    }
+                  />
+                  <NativeSelect
+                    data={[
+                      { value: "newest-to-oldest", label: "Newest to oldest" },
+                      { value: "oldest-to-newest", label: "Oldest to newest" },
+                      { value: "chronological", label: "Chronological" },
+                    ]}
+                    value={sort}
+                    variant="filled"
+                    onChange={(e) =>
+                      setSort(e.currentTarget.value as SortCategories)
+                    }
+                  />
+                </Group>
+                <ScrollArea h={420}>
+                  <AsideNotes
+                    mediaRef={mediaRef}
+                    transcriptContainerDivRef={transcriptContainerDivRef}
+                    notes={filteredNotes}
+                  />
+                  {searchTerm.length > 0 && filteredNotes.length === 0 && (
+                    <Stack h={"100%"} spacing={"lg"} align="center" mt={"md"}>
+                      <Text color="dimmed">No matching notes found...</Text>
+                    </Stack>
+                  )}
+                </ScrollArea>
+                {filteredNotes.length > 0 && (
+                  <>
+                    <Divider mt={"md"} />
+                    {/* @TODO implement import and export features */}
+                    <Group position="apart">
+                      <Button
+                        compact
+                        variant="subtle"
+                        leftIcon={<IconUpload size={16} />}
+                      >
+                        Import
+                      </Button>
+                      <Button
+                        compact
+                        variant="subtle"
+                        leftIcon={<IconDownload size={16} />}
+                      >
+                        Export
+                      </Button>
+                    </Group>
+                  </>
                 )}
+              </>
+            ) : (
+              <Stack h={"60%"} justify="center">
+                <EmptyState
+                  description="Start by selecting some text in the transcript and adding a note."
+                  imageUrl="/empty-state-images/files/empty-file.svg"
+                  title="No notes yet..."
+                />
+              </Stack>
+            )}
+          </>
+        )}
+
+        {segment === "tags" && (
+          <>
+            {tags.length > 0 ? (
+              <ScrollArea h={"100%"}>
+                <AsideTags
+                  tags={tags}
+                  setTags={setTags}
+                  projectId={projectId}
+                  user={user}
+                />
               </ScrollArea>
-            </>
-          ) : (
-            <Stack h={"60%"} justify="center">
-              <EmptyState
-                description="Start by selecting some text in the transcript and adding a note."
-                imageUrl="/empty-state-images/files/empty-file.svg"
-                title="No notes yet..."
-              />
-            </Stack>
-          )}
-        </>
-      )}
+            ) : (
+              <Stack h={"60%"} justify="center">
+                <EmptyState
+                  description="Start by selecting some text in the transcript and adding a note."
+                  imageUrl="/empty-state-images/files/empty-file.svg"
+                  title="No notes yet..."
+                />
+              </Stack>
+            )}
+          </>
+        )}
 
-      {segment === "tags" && (
-        <>
-          {tags.length > 0 ? (
-            <ScrollArea h={"100%"}>
-              <AsideTags
-                tags={tags}
-                setTags={setTags}
-                projectId={projectId}
-                user={user}
-              />
-            </ScrollArea>
-          ) : (
-            <Stack h={"60%"} justify="center">
-              <EmptyState
-                description="Start by selecting some text in the transcript and adding a note."
-                imageUrl="/empty-state-images/files/empty-file.svg"
-                title="No notes yet..."
-              />
-            </Stack>
-          )}
-        </>
-      )}
-
-      {segment === "chat" && <h1>chat</h1>}
+        {segment === "chat" && <h1>chat</h1>}
+      </Stack>
     </Aside>
   );
 };
