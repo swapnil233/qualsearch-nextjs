5b61478cc434b2606433783bf297fa2ca351dc60
Hasan Iqbal <swapniliqbal@gmail.com>
2023-10-21 13:20:02 -0400

    refactor: break StatsGrid component into two components, and memoize it for better performance

--- components/stats/StatsGrid.tsx
+++ components/stats/StatsGrid.tsx
@@ -1,119 +1,73 @@
 import { useNotes } from "@/contexts/NotesContext";
-import {
-  Box,
-  Group,
-  Paper,
-  SimpleGrid,
-  Text,
-  useMantineTheme,
-} from "@mantine/core";
-import {
-  IconHourglassHigh,
-  IconMessage,
-  IconTag,
-  IconUsers,
-} from "@tabler/icons-react";
-import { useEffect, useMemo, useState } from "react";
-
-type IconKey = "duration" | "notes" | "tags" | "contributors";
-
-const icons = {
-  duration: IconHourglassHigh,
-  notes: IconMessage,
-  tags: IconTag,
-  contributors: IconUsers,
-};
+import { Box, SimpleGrid } from "@mantine/core";
+import { FC, useEffect, useMemo, useState } from "react";
+import Stat, { ICONS } from "./Stat";
 
 interface StatsGridProps {
   duration: string;
 }
 
-interface StatData {
-  title: string;
-  icon: IconKey;
-  value: number | string;
-}
+const GRID_BREAKPOINTS = [
+  { maxWidth: "md", cols: 2 },
+  { maxWidth: "xs", cols: 1 },
+];
 
-export function StatsGrid({ duration }: StatsGridProps) {
-  const theme = useMantineTheme();
+const StatsGrid: FC<StatsGridProps> = ({ duration }) => {
   const { notes } = useNotes();
   const [tagsCount, setTagsCount] = useState<number>(0);
 
-  // Get the number of unique tags being used
+  // Calculate unique tags
   useEffect(() => {
-    setTagsCount(
-      new Set(notes.flatMap((note) => note.tags.map((tag) => tag.id))).size
+    const uniqueTags = new Set(
+      notes.flatMap((note) => note.tags.map((tag) => tag.id))
     );
+    setTagsCount(uniqueTags.size);
   }, [notes]);
 
-  // Get the number of contributors
-  let contributorsCount: number = useMemo(() => {
+  // Calculate unique contributors
+  const contributorsCount = useMemo(() => {
     return new Set(notes.map((note) => note.createdByUserId)).size;
   }, [notes]);
 
-  const data: StatData[] = [
-    {
-      title: "Duration",
-      icon: "duration",
-      value: duration,
-    },
-    {
-      title: "Notes",
-      icon: "notes",
-      value: notes.length,
-    },
-    {
-      title: "Tags used",
-      icon: "tags",
-      value: tagsCount,
-    },
-    {
-      title: "Contributors",
-      icon: "contributors",
-      value: contributorsCount,
-    },
-  ];
-
-  const stats = data.map((stat) => {
-    const Icon = icons[stat.icon];
-
-    return (
-      <Paper withBorder p="md" key={stat.title}>
-        <Group position="apart">
-          <Text size="xs" color="dimmed" fw={700} tt={"capitalize"}>
-            {stat.title}
-          </Text>
-          <Icon
-            color={
-              theme.colorScheme === "dark"
-                ? theme.colors.dark[3]
-                : theme.colors.gray[4]
-            }
-            size="1.4rem"
-            stroke={1.5}
-          />
-        </Group>
-
-        <Group align="flex-end" spacing="xs" mt={25}>
-          <Text fz={"1.5rem"} fw={400} lh={1}>
-            {stat.value}
-          </Text>
-        </Group>
-      </Paper>
-    );
-  });
+  const data: Array<{
+    title: string;
+    icon: keyof typeof ICONS;
+    value: string | number;
+  }> = useMemo(
+    () => [
+      {
+        title: "Duration",
+        icon: "duration",
+        value: duration,
+      },
+      {
+        title: "Notes",
+        icon: "notes",
+        value: notes.length,
+      },
+      {
+        title: "Tags used",
+        icon: "tags",
+        value: tagsCount,
+      },
+      {
+        title: "Contributors",
+        icon: "contributors",
+        value: contributorsCount,
+      },
+    ],
+    [duration, notes.length, tagsCount, contributorsCount]
+  );
 
   return (
-    <Box mt={"lg"}>
-      <SimpleGrid
-        cols={4}
-        breakpoints={[
-          { maxWidth: "md", cols: 2 },
-          { maxWidth: "xs", cols: 1 },
-        ]}
-      >
-        {stats}
+    <Box mt="lg">
+      <SimpleGrid cols={4} breakpoints={GRID_BREAKPOINTS}>
+        {data.map((stat) => (
+          <Stat key={stat.title} {...stat} />
+        ))}
       </SimpleGrid>
     </Box>
   );
-}
+};
+
+export default StatsGrid;
