97992049653381a02ce74c69d1793b9e8b507b47
Hasan Iqbal <swapniliqbal@gmail.com>
2023-10-30 10:11:34 -0400

    feat: updated landing page meta tags

--- components/layout/home/HomePageLayout.tsx
+++ components/layout/home/HomePageLayout.tsx
@@ -11,11 +11,26 @@ const HomePageLayout: FC<IHomePageLayout> = ({ children }) => {
   return (
     <>
       <Head>
-        <title>Transcription</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <meta name="description" content="Transcribe videos" />
-        <meta name="keywords" content="transcribe, diarize" />
+        <title>QualSearch</title>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+        <link
+          rel="canonical"
+          href="https://main.dvws5ww9zrzf5.amplifyapp.com"
+        />
+        <meta
+          name="description"
+          content="Steamlined UX Research With the Power of AI. Collaborate with your team to easily analyze user interviews using transcript tagging and AI powered insights."
+        />
+        <meta
+          name="keywords"
+          content="transcribe, diarize, CAQDAS, qualitative research, UX research analysis, UX research repository"
+        />
         <meta name="author" content="Hasan Iqbal" />
+        <meta name="copyright" content="QualSearch" />
       </Head>
       <Navbar />
       <main>{children}</main>
