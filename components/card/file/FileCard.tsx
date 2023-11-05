d1e02d159c44b0e737df973f6b6aeae402ea5820
Hasan Iqbal <swapniliqbal@gmail.com>
2023-11-05 02:09:07 -0500

    feat: show participant and participant's org info, add small hover animation, remove file type indicator

--- components/card/file/FileCard.tsx
+++ components/card/file/FileCard.tsx
@@ -1,169 +1,110 @@
 import { FileWithoutTranscriptAndUri } from "@/types";
-import timeAgo from "@/utils/timeAgo";
 import {
-  Avatar,
-  Badge,
+  ActionIcon,
   Button,
   Card,
   Group,
   Loader,
-  MantineColor,
+  Menu,
   Stack,
   Text,
 } from "@mantine/core";
-import { FileType } from "@prisma/client";
+import { useHover } from "@mantine/hooks";
 import {
-  IconFileText,
-  IconFileUnknown,
-  IconHeadphones,
-  IconPdf,
-  IconPhoto,
-  IconPresentation,
-  IconVideo,
+  IconBuilding,
+  IconCalendar,
+  IconDots,
+  IconEdit,
+  IconTrash,
+  IconUser,
 } from "@tabler/icons-react";
 import { useRouter } from "next/router";
-import React, { FC, memo, useMemo } from "react";
+import { FC, memo } from "react";
 
 export interface IFileCard {
   file: FileWithoutTranscriptAndUri;
   teamId: string;
 }
 
-interface IBadgeWithIcon {
-  fileType: string;
-  icon: React.ReactNode;
-  color?: MantineColor;
-}
-
-const BadgeWithIcon: FC<IBadgeWithIcon> = ({ fileType, icon, color }) => {
-  return (
-    <Badge
-      pl={0}
-      py={"0.5rem"}
-      size="sm"
-      color={color}
-      radius="md"
-      leftSection={
-        <Avatar variant="" color={color}>
-          {icon}
-        </Avatar>
-      }
-    >
-      <Text>{fileType}</Text>
-    </Badge>
-  );
-};
-
-const TypeToIcon = (ft: FileType) => {
-  switch (ft) {
-    case "AUDIO":
-      return (
-        <BadgeWithIcon
-          color="blue"
-          fileType="Audio"
-          icon={<IconHeadphones size={"1rem"} />}
-        />
-      );
-    case "VIDEO":
-      return (
-        <BadgeWithIcon
-          color="green"
-          fileType="Video"
-          icon={<IconVideo size={"1rem"} />}
-        />
-      );
-    case "EXCEL":
-      return (
-        <BadgeWithIcon
-          color="blue"
-          fileType="Excel"
-          icon={<IconFileText size={"1rem"} />}
-        />
-      );
-    case "PDF":
-      return (
-        <BadgeWithIcon
-          color="blue"
-          fileType="PDF"
-          icon={<IconPdf size={"1.2rem"} />}
-        />
-      );
-    case "POWERPOINT":
-      return (
-        <BadgeWithIcon
-          color="blue"
-          fileType="Powerpoint"
-          icon={<IconPresentation size={"1.2rem"} />}
-        />
-      );
-    case "WORD":
-      return (
-        <BadgeWithIcon
-          color="blue"
-          fileType="Word"
-          icon={<IconFileText size={"1.2rem"} />}
-        />
-      );
-    case "IMAGE":
-      return (
-        <BadgeWithIcon
-          color="blue"
-          fileType="Image"
-          icon={<IconPhoto size={"1.2rem"} />}
-        />
-      );
-    case "OTHER":
-      return (
-        <BadgeWithIcon
-          color="blue"
-          fileType="Other"
-          icon={<IconFileUnknown size={"1.2rem"} />}
-        />
-      );
-    default:
-      return null;
-  }
-};
-
 const FileCard: FC<IFileCard> = ({ file, teamId }) => {
   const router = useRouter();
-  const uploadedTimeAgo = useMemo(
-    () => timeAgo(new Date(file.updatedAt)),
-    [file.updatedAt]
-  );
+  const { hovered, ref } = useHover();
 
   return (
     <Card
+      ref={ref}
       withBorder
+      {...(hovered && { shadow: "xs" })}
       radius="sm"
       sx={(theme) => ({
         backgroundColor:
           theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.white,
+        transition: "all 0.1s ease",
       })}
     >
-      <Stack justify="space-between" align="stretch" h="100%">
+      <Stack justify="space-between" align="stretch" h="100%" spacing={"xl"}>
         <Stack spacing={"xs"} justify="space-between" align="stretch">
-          <Group w="100%" align="center" position="apart" noWrap>
+          <Group noWrap position="apart">
             <Group spacing={"xs"}>
               {file.status === "PROCESSING" && <Loader size={"xs"} />}
               <Text fz="lg" fw={500}>
                 {file.name}
               </Text>
             </Group>
-            {TypeToIcon(file.type)}
+            <Menu shadow="md" width={200}>
+              <Menu.Target>
+                <ActionIcon variant="transparent">
+                  <IconDots size="1rem" />
+                </ActionIcon>
+              </Menu.Target>
+              <Menu.Dropdown>
+                <Menu.Item icon={<IconEdit size={14} />}>Edit</Menu.Item>
+                <Menu.Item color="red" icon={<IconTrash size={14} />}>
+                  Delete
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
           </Group>
           <Text fz="sm" c={"dimmed"} lineClamp={2}>
             {file.description}
           </Text>
         </Stack>
 
-        <Group position="apart">
-          <Text fz="sm" c={"dimmed"}>
-            Updated {uploadedTimeAgo}
-          </Text>
+        <Group position="apart" noWrap>
+          <Group>
+            {file.participantName && (
+              <Group spacing={4} noWrap>
+                <IconUser size={"1.1rem"} color="gray" />
+                <Text fz="sm" c={"dimmed"} truncate lineClamp={1} maw={"100%"}>
+                  {file.participantName.trim()}
+                </Text>
+              </Group>
+            )}
+
+            {file.participantOrganization && (
+              <Group spacing={4} noWrap>
+                <IconBuilding size={"1.1rem"} color="gray" />
+                <Text fz="sm" c={"dimmed"} truncate lineClamp={1} maw={"100%"}>
+                  {file.participantOrganization.trim()}
+                </Text>
+              </Group>
+            )}
+
+            {file.dateConducted && (
+              <Group spacing={4} noWrap>
+                <IconCalendar size={"1.1rem"} color="gray" />
+                <Text fz="sm" c={"dimmed"} truncate lineClamp={1} maw={"100%"}>
+                  {new Date(file.dateConducted).toLocaleDateString("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                  })}{" "}
+                </Text>
+              </Group>
+            )}
+          </Group>
           <Button
             disabled={file.status === "PROCESSING"}
-            // loading={file.status === "PROCESSING"}
             variant="default"
             onClick={() => {
               file.status === "COMPLETED"
