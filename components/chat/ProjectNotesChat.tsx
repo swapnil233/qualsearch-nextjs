83023b103cca081639c199578a278371781491a0
Hasan Iqbal <swapniliqbal@gmail.com>
2023-11-12 10:49:58 -0500

    refactor: create component for ProjectNotesChat instead of having it live in the projects page

---
+++ components/chat/ProjectNotesChat.tsx
@@ -0,0 +1,229 @@
+import ChatSuggestionButton from "@/components/buttons/ChatSuggestionButton";
+import { ApiMessage, IMessageState, Message, UserMessage } from "@/types";
+import {
+  Button,
+  Group,
+  ScrollArea,
+  Stack,
+  Text,
+  Textarea,
+} from "@mantine/core";
+import { notifications } from "@mantine/notifications";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { FormEvent, KeyboardEvent, useRef, useState } from "react";
+
+interface ProjectNotesChatProps {
+  projectId: string;
+}
+
+const ProjectNotesChat: React.FC<ProjectNotesChatProps> = ({ projectId }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const { data: session } = useSession();
+  const [query, setQuery] = useState<string>("");
+  const [messageState, setMessageState] = useState<IMessageState>({
+    messages: [
+      {
+        message: "Ask me anything about the notes, tags, interviews, etc.",
+        type: "apiMessage",
+      },
+    ],
+    history: [],
+  });
+
+  const messageListRef = useRef<HTMLDivElement>(null);
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleMessageSubmit = async (
+    e: FormEvent,
+    suggestedMessage?: string
+  ) => {
+    e.preventDefault();
+
+    const messageToSend = suggestedMessage || query.trim();
+
+    if (!messageToSend) {
+      notifications.show({
+        title: "Please enter a question",
+        message: "You can't send an empty message",
+        color: "red",
+      });
+      return;
+    }
+
+    const newMessage: UserMessage = {
+      type: "userMessage",
+      message: messageToSend,
+    };
+
+    updateMessages(newMessage);
+    setLoading(true);
+    setQuery("");
+
+    // Send the messages to the API
+    try {
+      const response = await fetch("/api/chat", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          question: newMessage.message,
+          history: messageState.history,
+          projectId: projectId,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (data.error) {
+        setError(data.error);
+        notifications.show({
+          title: "An error occurred",
+          message: data.error,
+          color: "red",
+        });
+      } else {
+        const apiMessage: ApiMessage = {
+          type: "apiMessage",
+          message: data.text,
+          sourceDocs: data.sourceDocuments,
+        };
+
+        updateMessages(apiMessage, newMessage.message, data.text);
+      }
+    } catch (error) {
+      setError("An error occurred while fetching the data. Please try again.");
+      notifications.show({
+        title: "An error occurred",
+        message: "An error occurred while fetching the data. Please try again.",
+        color: "red",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSuggestionSubmit = (suggestion: string) => {
+    return (e: FormEvent) => handleMessageSubmit(e, suggestion);
+  };
+
+  const updateMessages = (
+    newMessage: Message,
+    question?: string,
+    answer?: string
+  ) => {
+    setMessageState((prevState) => ({
+      ...prevState,
+      messages: [...prevState.messages, newMessage],
+      history:
+        question && answer
+          ? [...prevState.history, [question, answer]]
+          : prevState.history,
+    }));
+
+    scrollToBottom();
+  };
+
+  const scrollToBottom = () => {
+    messageListRef.current?.scrollTo(0, messageListRef.current.scrollHeight);
+  };
+
+  const handleEnterPress = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      if (query) {
+        handleMessageSubmit(e as unknown as FormEvent);
+      }
+    }
+  };
+
+  return (
+    <Stack justify="space-between">
+      <Stack>
+        <Text weight={500}>Chat with QualSearch AI about your notes...</Text>
+        <ScrollArea h={"24rem"} ref={messageListRef}>
+          <Stack spacing={"lg"}>
+            {messageState.messages.map((message, index) => (
+              <Group
+                noWrap
+                key={`chatMessage-${index}`}
+                spacing={"xs"}
+                align="start"
+              >
+                <Image
+                  src={
+                    message.type === "apiMessage"
+                      ? "/bot.png"
+                      : session?.user?.image || "/user-image.png"
+                  }
+                  alt={
+                    message.type === "apiMessage"
+                      ? "AI"
+                      : `The profile picture of the user ${session?.user?.name}`
+                  }
+                  width="30"
+                  height="30"
+                  priority
+                />
+                <Stack spacing={4}>
+                  <Text weight={500}>
+                    {message.type === "apiMessage"
+                      ? "QualSearch AI"
+                      : `${session?.user?.name || "You"}`}
+                  </Text>
+                  <Text style={{ whiteSpace: "pre-wrap" }}>
+                    {message.message}
+                  </Text>
+                </Stack>
+              </Group>
+            ))}
+          </Stack>
+        </ScrollArea>
+      </Stack>
+      <Stack>
+        {messageState.messages.length === 1 && (
+          <Stack spacing={"xs"}>
+            <ChatSuggestionButton
+              suggestion={"What were the major pain points?"}
+              handleSubmit={handleSuggestionSubmit(
+                "What were the major pain points?"
+              )}
+            />
+            <ChatSuggestionButton
+              suggestion={"How can the product be improved?"}
+              handleSubmit={handleSuggestionSubmit(
+                "How can the product be improved?"
+              )}
+            />
+          </Stack>
+        )}
+        <form onSubmit={handleMessageSubmit}>
+          <Stack spacing={"xs"}>
+            <Textarea
+              disabled={loading}
+              onKeyDown={handleEnterPress}
+              ref={textAreaRef}
+              autoFocus={false}
+              minRows={4}
+              maxLength={512}
+              id="userInput"
+              name="userInput"
+              placeholder={
+                loading
+                  ? "Waiting for response..."
+                  : "What trends do you notice about..."
+              }
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <Button type="submit" loading={loading}>
+              Ask
+            </Button>
+          </Stack>
+        </form>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default ProjectNotesChat;
--- pages/teams/[teamId]/projects/[projectId]/index.tsx
+++ pages/teams/[teamId]/projects/[projectId]/index.tsx
@@ -1,6 +1,5 @@
-import { IMessageState } from "@/components/aside/AsideAiChat";
-import ChatSuggestionButton from "@/components/buttons/ChatSuggestionButton";
 import FileCard from "@/components/card/file/FileCard";
+import ProjectNotesChat from "@/components/chat/ProjectNotesChat";
 import PageHeading from "@/components/layout/heading/PageHeading";
 import PrimaryLayout from "@/components/layout/primary/PrimaryLayout";
 import DeleteNoteModal from "@/components/modal/delete/DeleteNoteModal";
@@ -13,13 +12,7 @@ import { useNoteDeletion } from "@/hooks/useNoteDeletion";
 import { useProjectDeletion } from "@/hooks/useProjectDeletion";
 import { validateUserIsTeamMember } from "@/infrastructure/services/team.service";
 import { NextPageWithLayout } from "@/pages/page";
-import {
-  ApiMessage,
-  FileWithoutTranscriptAndUri,
-  Message,
-  NoteWithTagsAndCreator,
-  UserMessage,
-} from "@/types";
+import { FileWithoutTranscriptAndUri, NoteWithTagsAndCreator } from "@/types";
 import { formatDatesToIsoString } from "@/utils/formatDatesToIsoString";
 import prisma from "@/utils/prisma";
 import { requireAuthentication } from "@/utils/requireAuthentication";
@@ -27,19 +20,13 @@ import {
   ActionIcon,
   Affix,
   Box,
-  Button,
-  Group,
   Popover,
-  ScrollArea,
   SimpleGrid,
   Stack,
-  Text,
-  Textarea,
   Title,
   rem,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { notifications } from "@mantine/notifications";
 import { Project } from "@prisma/client";
 import {
   IconFilePlus,
@@ -48,10 +35,8 @@ import {
   IconTrash,
 } from "@tabler/icons-react";
 import { GetServerSidePropsContext } from "next";
-import { useSession } from "next-auth/react";
 import Head from "next/head";
-import Image from "next/image";
-import { FormEvent, KeyboardEvent, useRef, useState } from "react";
+import { useState } from "react";
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   return requireAuthentication(context, async (session: any) => {
@@ -159,11 +144,8 @@ const ProjectPage: NextPageWithLayout<IProjectPage> = ({
   initialFiles,
   initialNotes,
 }) => {
-  const { data: session } = useSession();
-  const [query, setQuery] = useState<string>("");
   const [notes, setNotes] = useState<NoteWithTagsAndCreator[]>(initialNotes);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+
   const [opened, { open, close }] = useDisclosure(false);
   const [files, setFiles] =
     useState<FileWithoutTranscriptAndUri[]>(initialFiles);
@@ -175,121 +157,6 @@ const ProjectPage: NextPageWithLayout<IProjectPage> = ({
   const [projectDeletionModalOpened, setProjectDeletionModalOpened] =
     useState(false);
   const [deletingProject, setDeletingProject] = useState<boolean>(false);
-
-  const [messageState, setMessageState] = useState<IMessageState>({
-    messages: [
-      {
-        message: "What would you like to know about this interview?",
-        type: "apiMessage",
-      },
-    ],
-    history: [],
-  });
-
-  const messageListRef = useRef<HTMLDivElement>(null);
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
-
-  const handleMessageSubmit = async (
-    e: FormEvent,
-    suggestedMessage?: string
-  ) => {
-    e.preventDefault();
-
-    const messageToSend = suggestedMessage || query.trim();
-
-    if (!messageToSend) {
-      notifications.show({
-        title: "Please enter a question",
-        message: "You can't send an empty message",
-        color: "red",
-      });
-      return;
-    }
-
-    const newMessage: UserMessage = {
-      type: "userMessage",
-      message: messageToSend,
-    };
-
-    updateMessages(newMessage);
-    setLoading(true);
-    setQuery("");
-
-    // Send the messages to the API
-    try {
-      const response = await fetch("/api/chat", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          question: newMessage.message,
-          history: messageState.history,
-          projectId: project.id,
-        }),
-      });
-
-      const data = await response.json();
-
-      if (data.error) {
-        setError(data.error);
-        notifications.show({
-          title: "An error occurred",
-          message: data.error,
-          color: "red",
-        });
-      } else {
-        const apiMessage: ApiMessage = {
-          type: "apiMessage",
-          message: data.text,
-          sourceDocs: data.sourceDocuments,
-        };
-
-        updateMessages(apiMessage, newMessage.message, data.text);
-      }
-    } catch (error) {
-      setError("An error occurred while fetching the data. Please try again.");
-      notifications.show({
-        title: "An error occurred",
-        message: "An error occurred while fetching the data. Please try again.",
-        color: "red",
-      });
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handleSuggestionSubmit = (suggestion: string) => {
-    return (e: FormEvent) => handleMessageSubmit(e, suggestion);
-  };
-
-  const updateMessages = (
-    newMessage: Message,
-    question?: string,
-    answer?: string
-  ) => {
-    setMessageState((prevState) => ({
-      ...prevState,
-      messages: [...prevState.messages, newMessage],
-      history:
-        question && answer
-          ? [...prevState.history, [question, answer]]
-          : prevState.history,
-    }));
-
-    scrollToBottom();
-  };
-
-  const scrollToBottom = () => {
-    messageListRef.current?.scrollTo(0, messageListRef.current.scrollHeight);
-  };
-
-  const handleEnterPress = (e: KeyboardEvent<HTMLTextAreaElement>) => {
-    if (e.key === "Enter" && !e.shiftKey) {
-      e.preventDefault();
-      if (query) {
-        handleMessageSubmit(e as unknown as FormEvent);
-      }
-    }
-  };
 
   const { form, creating, buttonText, handleCreateNewFile } = useFileCreation(
     project.teamId,
@@ -467,93 +334,7 @@ const ProjectPage: NextPageWithLayout<IProjectPage> = ({
               </ActionIcon>
             </Popover.Target>
             <Popover.Dropdown>
-              <Stack justify="space-between">
-                <Stack>
-                  <Text weight={500}>
-                    Chat with QualSearch AI about your notes...
-                  </Text>
-                  <ScrollArea h={"32rem"} ref={messageListRef}>
-                    <Stack spacing={"lg"}>
-                      {messageState.messages.map((message, index) => (
-                        <Group
-                          noWrap
-                          key={`chatMessage-${index}`}
-                          spacing={"xs"}
-                          align="start"
-                        >
-                          <Image
-                            src={
-                              message.type === "apiMessage"
-                                ? "/bot.png"
-                                : session?.user?.image || "/user-image.png"
-                            }
-                            alt={
-                              message.type === "apiMessage"
-                                ? "AI"
-                                : `The profile picture of the user ${session?.user?.name}`
-                            }
-                            width="30"
-                            height="30"
-                            priority
-                          />
-                          <Stack spacing={4}>
-                            <Text weight={500}>
-                              {message.type === "apiMessage"
-                                ? "QualSearch AI"
-                                : `${session?.user?.name || "You"}`}
-                            </Text>
-                            <Text style={{ whiteSpace: "pre-wrap" }}>
-                              {message.message}
-                            </Text>
-                          </Stack>
-                        </Group>
-                      ))}
-                    </Stack>
-                  </ScrollArea>
-                </Stack>
-                <Stack>
-                  {messageState.messages.length === 1 && (
-                    <Stack spacing={"xs"}>
-                      <ChatSuggestionButton
-                        suggestion={"What were the major pain points?"}
-                        handleSubmit={handleSuggestionSubmit(
-                          "What were the major pain points?"
-                        )}
-                      />
-                      <ChatSuggestionButton
-                        suggestion={"How can the product be improved?"}
-                        handleSubmit={handleSuggestionSubmit(
-                          "How can the product be improved?"
-                        )}
-                      />
-                    </Stack>
-                  )}
-                  <form onSubmit={handleMessageSubmit}>
-                    <Stack spacing={"xs"}>
-                      <Textarea
-                        disabled={loading}
-                        onKeyDown={handleEnterPress}
-                        ref={textAreaRef}
-                        autoFocus={false}
-                        minRows={4}
-                        maxLength={512}
-                        id="userInput"
-                        name="userInput"
-                        placeholder={
-                          loading
-                            ? "Waiting for response..."
-                            : "What trends do you notice about..."
-                        }
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                      />
-                      <Button type="submit" loading={loading}>
-                        Ask
-                      </Button>
-                    </Stack>
-                  </form>
-                </Stack>
-              </Stack>
+              <ProjectNotesChat projectId={project.id} />
             </Popover.Dropdown>
           </Popover>
         </Affix>
