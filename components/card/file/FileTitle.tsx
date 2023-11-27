b63b4253c6321c41261c249f41f6bbe222f5aa33
Hasan Iqbal <swapniliqbal@gmail.com>
2023-11-27 16:08:47 -0500

    feat: implement component for file's title, which accounts for the file transcription status

---
+++ components/card/file/FileTitle.tsx
@@ -0,0 +1,20 @@
+import { Group, Loader, Text } from "@mantine/core";
+import { FileStatus } from "@prisma/client";
+
+export interface IFileTitleProps {
+  title: string;
+  status: FileStatus;
+}
+
+const FileTitle: React.FC<IFileTitleProps> = ({ title, status }) => {
+  return (
+    <Group spacing={"xs"}>
+      {status === "PROCESSING" && <Loader size={"xs"} />}
+      <Text fz="lg" fw={500}>
+        {title}
+      </Text>
+    </Group>
+  );
+};
+
+export default FileTitle;
