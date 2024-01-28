26ecc4000ade33e83401e980a3d5e4cb2e9bb093
Hasan Iqbal <swapniliqbal@gmail.com>
2024-01-28 03:58:41 -0500

    feat: create http 500 page (internal server error)

---
+++ pages/500.tsx
@@ -0,0 +1,96 @@
+import PrimaryLayout from "@/components/layout/primary/PrimaryLayout";
+import {
+  Button,
+  Container,
+  Group,
+  Text,
+  Title,
+  createStyles,
+  rem,
+} from "@mantine/core";
+import Head from "next/head";
+import Link from "next/link";
+import { NextPageWithLayout } from "./page";
+
+const useStyles = createStyles((theme) => ({
+  root: {
+    paddingTop: rem(80),
+    paddingBottom: rem(80),
+  },
+
+  label: {
+    textAlign: "center",
+    fontWeight: 900,
+    fontSize: rem(220),
+    lineHeight: 1,
+    marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[4]
+        : theme.colors.gray[2],
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: rem(120),
+    },
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    textAlign: "center",
+    fontWeight: 900,
+    fontSize: rem(38),
+
+    [theme.fn.smallerThan("sm")]: {
+      fontSize: rem(32),
+    },
+  },
+
+  description: {
+    maxWidth: rem(500),
+    margin: "auto",
+    marginTop: theme.spacing.xl,
+    marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
+  },
+}));
+
+const NotFound: NextPageWithLayout = () => {
+  const { classes } = useStyles();
+
+  return (
+    <>
+      <Head>
+        <title>QualSearch | Error 500!</title>
+        <meta name="description" content="500! Internal server error." />
+      </Head>
+      <Container className={classes.root}>
+        <div className={classes.label}>500</div>
+        <Title className={classes.title}>
+          Error 500: Internal Server Error
+        </Title>
+        <Text
+          color="dimmed"
+          size="lg"
+          align="center"
+          className={classes.description}
+        >
+          Sorry, an error has occurred, and we are working to fix it. Please
+          contact help@qualsearch.io if the problem persists. Thank you for your
+          patience.
+        </Text>
+        <Group position="center">
+          <Link href="/">
+            <Button variant="default" size="md">
+              Take me home
+            </Button>
+          </Link>
+        </Group>
+      </Container>
+    </>
+  );
+};
+
+export default NotFound;
+
+NotFound.getLayout = (page) => {
+  return <PrimaryLayout>{page}</PrimaryLayout>;
+};
