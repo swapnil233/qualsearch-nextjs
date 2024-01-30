4289d805a99afb5d2a9107e94ec74e4235a429ec
Hasan Iqbal <swapniliqbal@gmail.com>
2024-01-30 16:06:14 -0500

    feat: configure CORS to allow requests to/from the seperate express API

--- next.config.js
+++ next.config.js
@@ -30,6 +30,23 @@ module.exports = {
           },
         ],
       },
+      {
+        // matching all API routes
+        source: `${process.env.EXPRESS_BACKEND_URL}/api/:path*`,
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
     ];
   },
 };
