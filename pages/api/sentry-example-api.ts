795830ffbf47d37f87464fef6668ddc581d6ec75
Hasan Iqbal <swapniliqbal@gmail.com>
2024-05-19 03:59:56 -0400

    refactor: rm unused API routes

--- pages/api/sentry-example-api.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-// A faulty API route to test Sentry's error monitoring
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: "John Doe" });
-  throw new Error("Sentry Example API Route Error");
-}
