d71bd318da44fa3957986d398b5403ae156b02ba
Hasan Iqbal <swapniliqbal@gmail.com>
2024-08-06 02:33:45 -0400

    bring in Roboto font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,25 @@ import useOnlineStatus from "@/hooks/useOnlineStatus";
 import "@/styles/globals.css";
 import { MantineProvider } from "@mantine/core";
 import { Notifications, notifications } from "@mantine/notifications";
+import "@mantine/notifications/styles.css";
 import { IconX } from "@tabler/icons-react";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
 import { useEffect } from "react";
 import { NextPageWithLayout } from "./page";
 
 interface AppPropsWithLayout extends AppProps {
   Component: NextPageWithLayout;
 }
+
+const roboto = Roboto({
+  subsets: ["latin-ext"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
 
 export default function App({
   Component,
@@ -46,7 +53,11 @@ export default function App({
       <MantineProvider withGlobalClasses defaultColorScheme="light">
         <SessionProvider session={session}>
           <RouterTransition />
-          {getLayout(<Component {...pageProps} />)}
+          {getLayout(
+            <main className={roboto.className}>
+              <Component {...pageProps} />
+            </main>
+          )}
           <Analytics />
           <SpeedInsights />
           <Notifications position="top-right" />
