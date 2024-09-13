0bec90c3108d703805ebac8b7ef955a242575a14
Hasan Iqbal <swapniliqbal@gmail.com>
2024-09-12 20:22:33 -0400

    use native mantine styles for hero page

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -1,8 +1,8 @@
 import useTeamRedirect from "@/hooks/useTeamRedirect";
 import {
-  Box,
   Button,
   Container,
+  Group,
   Text,
   Title,
   useMantineColorScheme,
@@ -18,56 +18,45 @@ export const Hero = () => {
   const { colorScheme } = useMantineColorScheme();
   const theme = useMantineTheme();
 
-  const textColor = colorScheme === "dark" ? "text-white" : "text-black";
-  const highlightColor =
-    theme.colors[theme.primaryColor][colorScheme === "dark" ? 4 : 6];
-
   return (
-    <div className="relative pt-30 pb-20 sm:pt-20 sm:pb-15">
-      <div className="relative z-10">
-        <Title
-          className={`text-center font-extrabold leading-tight text-6xl tracking-tight mb-2 ${textColor} font-greycliff sm:text-5xl`}
+    <div className="relative z-10">
+      <Title
+        ta="center"
+        fw={800}
+        lh={1.1}
+        fz="60px"
+        lts={-1}
+        c={colorScheme === "light" ? theme.black : theme.white}
+        mb="xs"
+      >
+        Streamlined UX Research
+        <br />
+        With the Power of AI.
+      </Title>
+
+      <Container p={20} size={600}>
+        <Text fz={24} c="dimmed" ta="center">
+          Collaborate with your team to easily analyze user interviews using
+          transcript tagging and AI powered insights.
+        </Text>
+      </Container>
+
+      <Group mt="md" justify="center">
+        <Button
+          size="lg"
+          variant="default"
+          leftSection={<IconPlayerPlay size={20} />}
         >
-          Streamlined{" "}
-          <Text component="span" className={`text-${highlightColor}`} inherit>
-            UX Research
-          </Text>{" "}
-          <br />
-          With the Power of AI.
-        </Title>
-
-        <Container p={20} size={600}>
-          <Text
-            size="lg"
-            color="dimmed"
-            className="w-full text-center text-2xl sm:text-xl"
-          >
-            Collaborate with your team to easily analyze user interviews using
-            transcript tagging and AI powered insights.
-          </Text>
-        </Container>
-
-        <Box className="mt-6 flex justify-center sm:flex-col">
-          <Button
-            className="mt-0 sm:mt-4 sm:ml-0 ml-4"
-            size="lg"
-            variant="default"
-            color="gray"
-            component="a"
-            leftSection={<IconPlayerPlay size={20} />}
-          >
-            Demo
-          </Button>
-          <Link
-            href={status === "unauthenticated" ? "/signin" : teamRedirectUrl}
-            className="mt-0 sm:mt-4 sm:ml-0 ml-4"
-          >
-            <Button className="w-full" size="lg">
-              Get started
-            </Button>
-          </Link>
-        </Box>
-      </div>
+          Demo
+        </Button>
+        <Button
+          size="lg"
+          component={Link}
+          href={status === "unauthenticated" ? "/signin" : teamRedirectUrl}
+        >
+          Get started
+        </Button>
+      </Group>
     </div>
   );
 };
