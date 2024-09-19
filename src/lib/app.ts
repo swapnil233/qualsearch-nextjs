8cc14b9ed00f829cfddc119c2baa571336164ca9
Hasan Iqbal <swapniliqbal@gmail.com>
2024-09-19 16:26:45 -0400

    feat: obj to define app details

---
+++ src/lib/app.ts
@@ -0,0 +1,16 @@
+import packageInfo from "../../package.json";
+
+const app = {
+  version: packageInfo.version,
+  author: "Hasan Iqbal",
+  name: "QualSearch",
+  description:
+    "Qualitative research analysis platform.",
+  logoUrl: {
+    dark: "/logo-dark.svg",
+    light: "/logo-light.svg"
+  },
+  logoUrlAlt: "QualSearch logo",
+};
+
+export default app;
